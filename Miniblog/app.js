fe450fed3d6b479415c5a18b02eae31371f5fce4
alftacmir <alum.ataconm.2021@iesalixar.org>
2023-02-06 19:58:57 +0100

    Miniblog con api funcional

--- Miniblog/app.js
+++ Miniblog/app.js
@@ -3,13 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var postsRouter = require("./routes/posts");
 
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
+mongoose.set('strictQuery', false); //requerido para quitar el warning
+mongoose.connect(process.env.DB_URI, { useNewURLParser: true }).then(() => console.log('connection successful')).catch((err) => console.error(err));
 
 var app = express();
+var db = mongoose.connection;
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/posts", postsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,5 +48,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- Miniblog/routes/users.js
+++ Miniblog/routes/users.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/User');
+var db = mongoose.connection;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET del listado de usuarios ordenados por fecha de creación
+router.get("/", function (req, res, next) {
+  User.find()
+    .sort("-creationdate")
+    .exec(function (err, users) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(users);
+    });
+});
+
+// GET de un único usuario por su Id
+router.get("/:id", function (req, res, next) {
+  User.findById(req.params.id, function (err, userinfo) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(userinfo);
+  });
+});
+
+// POST de un nuevo usuario
+router.post("/", function (req, res, next) {
+  User.create(req.body, function (err, userinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
 });
 
 module.exports = router;
